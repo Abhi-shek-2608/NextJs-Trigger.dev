9e226a46d806591f4b69a88f97257920fda8674f
Abhishek Mahata <abhishek@Abhisheks-MacBook-Pro.local>
2025-08-05 12:14:41 -0500

    task added

--- src/pages/api/github-webhook.ts
+++ src/pages/api/github-webhook.ts
@@ -29,8 +29,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     // Trigger the Trigger.dev task
     await tasks.trigger<typeof githubPushLoggerTask>("github-push-logger", payload);
     console.log("Task triggereddd");
-    return res.status(200).json({ message: "Task triggered" });
-    console.log("Task triggered101");
+    return res.status(200).json({ message: "Task triggered" })
   } catch (error) {
     console.error("Error processing webhook:", error);
     return res.status(500).json({ message: "Internal Server Error" });
